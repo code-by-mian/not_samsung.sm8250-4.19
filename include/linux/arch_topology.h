90c64f65e9bffdd59575c29de9f9bdad6ca814b0
Ionela Voinescu <ionela.voinescu@arm.com>
2020-09-24 13:30:15 +0100

    cpufreq,arm,arm64: restructure definitions of arch_set_freq_scale()
    Compared to other arch_* functions, arch_set_freq_scale() has an atypical
    weak definition that can be replaced by a strong architecture specific
    implementation.
    
    The more typical support for architectural functions involves defining
    an empty stub in a header file if the symbol is not already defined in
    architecture code. Some examples involve:
     - #define arch_scale_freq_capacity	topology_get_freq_scale
     - #define arch_scale_freq_invariant	topology_scale_freq_invariant
     - #define arch_scale_cpu_capacity	topology_get_cpu_scale
     - #define arch_update_cpu_topology	topology_update_cpu_topology
     - #define arch_scale_thermal_pressure	topology_get_thermal_pressure
     - #define arch_set_thermal_pressure	topology_set_thermal_pressure
    
    Bring arch_set_freq_scale() in line with these functions by renaming it to
    topology_set_freq_scale() in the arch topology driver, and by defining the
    arch_set_freq_scale symbol to point to the new function for arm and arm64.
    
    While there are other users of the arch_topology driver, this patch defines
    arch_set_freq_scale for arm and arm64 only, due to their existing
    definitions of arch_scale_freq_capacity. This is the getter function of the
    frequency invariance scale factor and without a getter function, the
    setter function - arch_set_freq_scale() has not purpose.
    
    Change-Id: I23f903c84d140dc4d88841821a577032afee89c7
    Signed-off-by: Ionela Voinescu <ionela.voinescu@arm.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Acked-by: Catalin Marinas <catalin.marinas@arm.com>
    Acked-by: Sudeep Holla <sudeep.holla@arm.com> (BL_SWITCHER and topology parts)
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: //. <174224977+skye-tachyon@users.noreply.github.com>

--- include/linux/arch_topology.h
+++ include/linux/arch_topology.h
@@ -63,6 +63,9 @@ void topology_set_thermal_pressure(const struct cpumask *cpus,
 void topology_update_thermal_pressure(const struct cpumask *cpus,
 				      unsigned long capped_freq);
 
+void topology_set_freq_scale(const struct cpumask *cpus, unsigned long cur_freq,
+			     unsigned long max_freq);
+
 bool topology_scale_freq_invariant(void);
 
 bool arch_freq_counters_available(const struct cpumask *cpus);
